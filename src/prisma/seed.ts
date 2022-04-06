899fc95c2d4c544f2ed30eb163b48171efb18c4f
João Santana <joaosb989@gmail.com>
2022-04-06 16:57:18 -0300

    Corrige migracao e seeds

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -4,17 +4,8 @@ import { transactions_array } from "../files/transactions";
 
 async function main() {
   for (const transaction of transactions_array) {
-    // TODO add data validation
-    const parseTransactionDate = new Date(transaction.transactionDate);
-    const parseCreatedAt = new Date(transaction.createdAt);
-    const parseUpdatedAt = new Date(transaction.updatedAt);
     await prisma.transactions.create({
-      data: {
-        ...transaction,
-        transactionDate: parseTransactionDate,
-        createdAt: parseCreatedAt,
-        updatedAt: parseUpdatedAt,
-      },
+      data: transaction,
     });
   }
 }
